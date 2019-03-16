f2dcdbd5f208e8524cc3f7ec9febd968dfc0b825
Mehmet Yılmaz <ylmazmehmet60@gmail.com>
2019-03-16 22:28:21 +0100

    CASE 30 - Ürünlerin gösterilmesi için Anasayfa yapilmasi

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,7 +9,33 @@ use App\Product;
 class IndexController extends Controller
 {
     public function index(){
-		$productsAll = Product::get();
-		return view('index')->with(compact('productsAll'));
-	}
+    	$productsAll = Product::get();
+    	$productsAll = json_decode(json_encode($productsAll));
+    	$categories_menu = "";
+    	$categories = Category::with('categories')->where(['parent_id' => 0])->get();
+    	$categories = json_decode(json_encode($categories));
+		foreach($categories as $cat){
+			$categories_menu .= "
+			<div class='panel-heading'>
+				<h4 class='panel-title'>
+					<a data-toggle='collapse' data-parent='#accordian' href='#".$cat->id."'>
+						<span class='badge pull-right'><i class='fa fa-plus'></i></span>
+						".$cat->name."
+					</a>
+				</h4>
+			</div>
+			<div id='".$cat->id."' class='panel-collapse collapse'>
+				<div class='panel-body'>
+					<ul>";
+					$sub_categories = Category::where(['parent_id' => $cat->id])->get();
+					foreach($sub_categories as $sub_cat){
+						$categories_menu .= "<li><a href='#'>".$sub_cat->name." </a></li>";
+					}
+						$categories_menu .= "</ul>
+				</div>
+			</div>
+			";		
+		}
+    	return view('index')->with(compact('productsAll','categories_menu','categories'));
+    }
 }
--- resources/views/layouts/frontLayout/front_sidebar.blade.php
+++ resources/views/layouts/frontLayout/front_sidebar.blade.php
@@ -3,25 +3,29 @@
 					<h2>Kategori</h2>
 						<div class="panel-group category-products" id="accordian">
 							<!--category-products-->
-							
+							@foreach($categories as $cat)
 							<div class="panel panel-default">
 								<div class="panel-heading">
 									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordian" href="#">
+										<a data-toggle="collapse" data-parent="#accordian" href="#{{$cat->id}}">
 											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
-											
+											{{$cat->name}}
 										</a>
 									</h4>
 								</div>
-								<div id="" class="panel-collapse collapse">
+								<div id="{{$cat->id}}" class="panel-collapse collapse">
 									<div class="panel-body">
 										<ul>
-											<li><a href=""></a></li>
+											@foreach($cat->categories as $subcat)
+												@if($subcat->status==1)
+												<li><a href="{{ asset('products/'.$subcat->url) }}">{{$subcat->name}} </a></li>
+												@endif
+											@endforeach
 										</ul>
 									</div>
 								</div>
 							</div>
-		
+							@endforeach
 
-						</div>
+						</div><!--/category-products-->
 					</div>
